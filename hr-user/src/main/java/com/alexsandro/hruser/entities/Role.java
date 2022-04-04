ba18782e9e23ec6a29ae0ce99588bb5f8efd2773
alex <alexsandro.sfo@gmail.com>
2022-04-04 00:09:41 -0300

    database seeding

--- hr-user/src/main/java/com/alexsandro/hruser/entities/Role.java
+++ hr-user/src/main/java/com/alexsandro/hruser/entities/Role.java
@@ -2,11 +2,11 @@ package com.alexsandro.hruser.entities;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.Objects;
 
 @Entity
 @Table(name = "tb_role")
 public class Role implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,6 +17,7 @@ public class Role implements Serializable {
     }
 
     public Role(Long id, String roleName) {
+        super();
         this.id = id;
         this.roleName = roleName;
     }
@@ -38,15 +39,27 @@ public class Role implements Serializable {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Role role = (Role) o;
-        return roleName.equals(role.roleName);
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((roleName == null) ? 0 : roleName.hashCode());
+        return result;
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(roleName);
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Role other = (Role) obj;
+        if (roleName == null) {
+            if (other.roleName != null)
+                return false;
+        } else if (!roleName.equals(other.roleName))
+            return false;
+        return true;
     }
 }
--- hr-user/src/main/java/com/alexsandro/hruser/entities/User.java
+++ hr-user/src/main/java/com/alexsandro/hruser/entities/User.java
@@ -9,26 +9,29 @@ import java.util.Set;
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String nome;
+    private String name;
     private String email;
     private String password;
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "tb_user_role",
             joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id"))
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
     private Set<Role> roles = new HashSet<>();
 
     public User() {
     }
 
-    public User(Long id, String nome, String email, String password) {
+    public User(Long id, String name, String email, String password) {
+        super();
         this.id = id;
-        this.nome = nome;
+        this.name = name;
         this.email = email;
         this.password = password;
     }
@@ -41,12 +44,12 @@ public class User implements Serializable {
         this.id = id;
     }
 
-    public String getNome() {
-        return nome;
+    public String getName() {
+        return name;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getEmail() {
@@ -67,5 +70,30 @@ public class User implements Serializable {
 
     public Set<Role> getRoles() {
         return roles;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 }
