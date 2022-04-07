08cd9ddcee64685fb6191eac8059af00af69dd3f
alex <alexsandro.sfo@gmail.com>
2022-04-06 22:12:02 -0300

    fixing projects dependencies

--- hr-worker/src/main/java/com/alexsandro/hrworker/repositories/WorkerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alexsandro.hrworker.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.alexsandro.hrworker.entities.Worker;
-
-public interface WorkerRepository extends JpaRepository<Worker, Long>{
-
-}
