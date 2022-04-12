d733803686bb54cea9b61e25b35dabba5f156814
alex <alexsandro.sfo@gmail.com>
2022-04-11 22:13:32 -0300

    Login, jwtToken

--- hr-oauth/src/main/java/com/alexsandro/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/alexsandro/hroauth/services/UserService.java
@@ -5,10 +5,13 @@ import com.alexsandro.hroauth.feignclients.UserFeignClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -25,4 +28,14 @@ public class UserService {
         return user;
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        User user = userFeignClient.findByEmail(userName).getBody();
+        if (user == null) {
+            logger.error("Email not found " + userName);
+            throw new UsernameNotFoundException("Email not found");
+        }
+        logger.info("Email found: " + userName);
+        return user;
+    }
 }
