2eec1dd4bb17f4a98689ffdeca3a790a3e0d24e6
alex <alexsandro.sfo@gmail.com>
2022-04-15 00:33:57 -0300

    security configs to config server

--- hr-api-gateway-zuul/src/main/java/com/alexsandro/hrapigatewayzuul/config/ResourceServerConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/alexsandro/hrapigatewayzuul/config/ResourceServerConfig.java
@@ -15,11 +15,10 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     private static final String[] PUBLIC = {"/hr-oauth/oauth/token"};
     private static final String[] OPERATOR = {"/hr-worker/**"};
-    private static final String[] ADMIN = {"/hr-payroll/**", "/hr-user/**"};
-
+    private static final String[] ADMIN = { "/hr-payroll/**", "/hr-user/**", "/actuator/**", "/hr-worker/actuator/**", "/hr-oauth/actuator/**" };
 
     @Autowired
-    private JwtTokenStore  tokenStore;
+    private JwtTokenStore tokenStore;
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
--- hr-oauth/src/main/java/com/alexsandro/hroauth/config/AppConfig.java
+++ hr-oauth/src/main/java/com/alexsandro/hroauth/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.alexsandro.hroauth.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,6 +11,8 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 public class AppConfig {
 
+    @Value("${jwt.secret}")
+    private String jwtSecret;
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
@@ -20,7 +23,7 @@ public class AppConfig {
     @Bean
     public JwtAccessTokenConverter accessTokenConverter(){
         JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-        tokenConverter.setSigningKey("myappsecret123");
+        tokenConverter.setSigningKey(jwtSecret);
         return tokenConverter;
     }
 
